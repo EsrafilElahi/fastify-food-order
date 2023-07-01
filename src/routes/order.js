7daec9a95cced2b8e327ec2e6b021c0a5f9122bd
esrafilelahi <essinho1121@gmail.com>
2023-07-01 19:18:33 +0330

    add food order user routes

---
+++ src/routes/order.js
@@ -0,0 +1,13 @@
+import Controller from "../controllers/order.js";
+
+export default function (fastify, opts, done) {
+  const controller = new Controller()
+
+  fastify.get('/', controller.GET)
+  fastify.get('/:id', controller.GET)
+  fastify.post('/', controller.POST)
+  fastify.put('/:id', controller.PUT)
+  fastify.delete('/:id', controller.DELETE)
+
+  done();
+}
